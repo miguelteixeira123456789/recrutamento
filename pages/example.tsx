dc60d9e06255ebd85b43ec226b5b926714e501fc
miguelteixeira123456789 <147928128+miguelteixeira123456789@users.noreply.github.com>
2023-10-14 17:09:36 +0100

    Add files via upload

--- pages/example.tsx
+++ pages/example.tsx
@@ -1,23 +1,84 @@
-import { useTodo } from "@/lib/frontend/hooks";
+// Todo.js
+import { useState } from "react";
 import { Button, Title } from "@mantine/core";
+import setImage from "pages/set.jpeg";
 
 export default function Todo() {
-  const { todos, addTodo } = useTodo(["Todo #1", "Todo #2", "Todo 3"]);
+  const [todos, setTodos] = useState(["Todo #1", "Todo #2", "Todo 3"]);
+  const [inputText, setInputText] = useState("");
+
+  const imageStyles = {
+    width: '200px', 
+    height: 'auto', 
+    display: 'block',
+    margin: '0 auto',
+    marginBottom: '20px',
+  };
+
+  const fontStyles = {
+    fontFamily: 'Ultra Fresh - Demo', 
+  };
+
+  const buttonStyles = {
+    fontFamily: 'Ultra Fresh - Demo',
+    backgroundColor: '#FF5733', 
+    color: '#FFFFFF',
+  };
+
+  const componentStyles = {
+    fontFamily: 'Ultra Fresh - Demo',
+  };
+
+  // Function to handle item deletion
+  const handleDelete = (index) => {
+    const updatedTodos = [...todos];
+    updatedTodos.splice(index, 1);
+    setTodos(updatedTodos);
+  };
+
+  // Function to handle item addition
+  const handleAdd = () => {
+    if (inputText) {
+      const updatedTodos = [...todos, inputText];
+      setTodos(updatedTodos);
+      setInputText("");
+    }
+  };
 
   return (
-    <div className="h-screen w-screen flex flex-col items-center justify-center">
-      <div className=" h-fit w-2/3 rounded-lg border">
-        <div className=" h-16 p-4 border-b flex flex-row items-center">
-          <Button onClick={addTodo}>Add new item</Button>
+    <div className="h-screen w-screen flex flex-col items-center justify-center bg-blue-200" style={componentStyles}>
+      <div className="w-4/5 max-w-md rounded-lg border shadow-lg p-4">
+        <Title order={2} className="text-center mb-4" style={fontStyles}>
+          SET - To Do List
+        </Title>
+        
+        <div style={{ textAlign: 'center', marginBottom: '20px' }}>
+          <img
+            src={setImage.src}
+            alt="SET Logo"
+            style={imageStyles}
+          />
         </div>
-        <div className=" h-96 p-4 flex flex-col gap-2 overflow-y-scroll">
+
+        <div className="p-2 border-b flex flex-row items-center mb-4">
+          <input
+            type="text"
+            placeholder="Add a new item..."
+            className="p-2 w-full rounded border focus:outline-none focus:ring focus:ring-blue-400"
+            value={inputText}
+            onChange={(e) => setInputText(e.target.value)}
+          />
+          <Button onClick={handleAdd} variant="filled" color="red" style={buttonStyles}>
+            Add
+          </Button>
+        </div>
+        <div className="p-4">
           {todos.map((todo, index) => (
-            <div
-              key={index}
-              className="w-full border rounded-lg flex flex-row items-center justify-between p-4"
-            >
-              <h2 className="text-xl font-semibold">{todo}</h2>
-              <Button bg="red">Delete</Button>
+            <div key={index} className="bg-white rounded-lg p-3 mb-4 flex flex-row items-center justify-between">
+              <h2 className="text-lg font-semibold" style={{ ...fontStyles }}>{todo}</h2>
+              <Button onClick={() => handleDelete(index)} variant="filled" color="red" style={buttonStyles}>
+                Delete
+              </Button>
             </div>
           ))}
         </div>
